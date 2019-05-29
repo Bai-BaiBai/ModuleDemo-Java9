6efe58deaf2006d1432fb77b815a50a83a70bd7b
BaiZeHong <bzhdlnu@163.com>
2019-05-29 20:49:30 +0000

    封装的Service异常类

---
+++ service/src/main/java/sms/service/ServiceException.java
@@ -0,0 +1,21 @@
+package sms.service;
+
+/**
+ * service层的自定义异常类
+ */
+public class ServiceException extends Exception {
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+}

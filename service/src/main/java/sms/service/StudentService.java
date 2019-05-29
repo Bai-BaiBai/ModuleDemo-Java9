598543953d4b2a74549f5c2e3925b4e9c9d92a8e
BaiZeHong <bzhdlnu@163.com>
2019-05-29 20:48:59 +0000

    Service接口

---
+++ service/src/main/java/sms/service/StudentService.java
@@ -0,0 +1,21 @@
+package sms.service;
+
+import sms.model.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService {
+
+    List<Student> list() throws ServiceException;
+
+    Optional<Student> getStudent(String id) throws ServiceException;
+
+    void addStudent(Student student) throws ServiceException;
+
+    void add(String id, String name, String group) throws ServiceException;
+
+    void updataStudent(Student student) throws ServiceException;
+
+    void deleteStudent(Student student) throws ServiceException;
+}
